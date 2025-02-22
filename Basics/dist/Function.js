f4a104e24e1570de2e7c028382384de21175c22c
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-02-22 16:30:18 +0530

    code updated

--- Basics/dist/Function.js
+++ Basics/dist/Function.js
@@ -4,8 +4,19 @@
 // }
 // let greet = Hello('Durgesh');
 // console.log(greet);
-function myName(fname, id, active) {
-    return `Hello ${fname} your id is ${id} and status is ${active}`;
+// function myName(fname: string, id: number, active: boolean): string | number | boolean{
+//     return `Hello ${fname} your id is ${id} and status is ${active}`;
+// }
+// let ans = myName("Durgesh", 69, true);
+// console.log(ans);
+// function isLegal(age: number) : boolean {
+//     return age >= 18 ? true : false;
+// }
+// let ans = isLegal(22);
+// console.log(ans);
+function first(fn) {
+    setTimeout(fn, 1000);
 }
-let ans = myName("Durgesh", 69, true);
-console.log(ans);
+first(function () {
+    console.log("Hello");
+});
