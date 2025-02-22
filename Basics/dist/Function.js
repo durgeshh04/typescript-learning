93f3bda5ed2b0ac689286ce529f295a6c7385359
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-02-22 15:57:12 +0530

    function syntax update

--- Basics/dist/Function.js
+++ Basics/dist/Function.js
@@ -1,6 +1,11 @@
 "use strict";
-function Hello(name) {
-    return `${name} hello, hope your dreams come true`;
+// function Hello(name:string) : string {
+//     return `${name} hello, hope your dreams come true`;
+// }
+// let greet = Hello('Durgesh');
+// console.log(greet);
+function myName(fname, id, active) {
+    return `Hello ${fname} your id is ${id} and status is ${active}`;
 }
-let greet = Hello('Durgesh');
-console.log(greet);
+let ans = myName("Durgesh", 69, true);
+console.log(ans);
--- Basics/src/Function.ts
+++ Basics/src/Function.ts
@@ -1,6 +1,15 @@
-function Hello(name:string) : string {
-    return `${name} hello, hope your dreams come true`;
+// function Hello(name:string) : string {
+//     return `${name} hello, hope your dreams come true`;
+// }
+
+// let greet = Hello('Durgesh');
+// console.log(greet);
+
+
+function myName(fname: string, id: number, active: boolean): string | number | boolean{
+    return `Hello ${fname} your id is ${id} and status is ${active}`;
+    
 }
 
-let greet = Hello('Durgesh');
-console.log(greet);
+let ans = myName("Durgesh", 69, true);
+console.log(ans);
