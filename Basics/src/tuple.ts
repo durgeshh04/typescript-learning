feac719979fd2bd4bb91c78ba4dc9d9a9488c6b3
Durgesh Chaudhari <133797261+durgeshh04@users.noreply.github.com>
2025-02-22 20:02:58 +0530

    code updated

---
+++ Basics/src/tuple.ts
@@ -0,0 +1,9 @@
+const arr : [number, string] = [1, "Apple"];
+// arr.push(true);
+
+
+
+// In previous version of ts we allow to add third valud without defining it's type like I've shown in syntax
+
+// But now in updated code we're not allowed to use and to implement it
+
